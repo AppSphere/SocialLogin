a7f7b89d0e957a1a753444d6d7e6d342d8619849
vishalisharani <vishal@appsphere.in>
2019-12-04 06:17:40 +0530

    config.xml issue fixes

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,61 +1,59 @@
-import { HttpHeaders, HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import {HttpHeaders, HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
 
-import { environment } from 'src/environments/environment';
-import { HttpProvider } from 'src/app/providers/http/http';
-import { TokenResponse } from 'src/app/viewmodels/token-response';
-import { HttpWrapperService } from '../http-wrapper.service'
-import { map, catchError } from 'rxjs/operators';
-import { of } from 'rxjs';
+import {environment} from 'src/environments/environment';
+import {HttpProvider} from 'src/app/providers/http/http';
+import {TokenResponse} from 'src/app/viewmodels/token-response';
+import {HttpWrapperService} from '../http-wrapper.service';
+import {map, catchError} from 'rxjs/operators';
+import {of} from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpWrapperService) {}
+    constructor(private http: HttpWrapperService) {
+    }
 
 
-  // API End Point /Account/login/facebook
+    // API End Point /Account/login/facebook
 
-  loginWithFacebook(accessToken, deviceId, deviceName) {
-    const endPoint = environment.apiEndPoint + '/Account/login/facebook';
-    const request = {
-      token: accessToken,
-      deviceId: deviceId,
-      deviceName: deviceName
+    loginWithFacebook(accessToken, deviceId, deviceName) {
+        const endPoint = environment.apiEndPoint + '/Account/login/facebook';
+        const request = {
+            token: accessToken,
+            deviceId: deviceId,
+            deviceName: deviceName
 
-    };
+        };
 
-  }
+    }
 
     // API End Point /Account/login/google (We should remove Redundant Calls If Required)
     loginWithGoogle(accessToken, deviceId, deviceName) {
-      const endPoint = environment.apiEndPoint + '/Account/login/google';
-      const request = {
-        token: accessToken,
-        deviceId: deviceId,
-        deviceName: deviceName
+        const endPoint = environment.apiEndPoint + '/Account/login/google';
+        const request = {
+            token: accessToken,
+            deviceId: deviceId,
+            deviceName: deviceName
 
-      };
-
-    
+        };
     }
-
 
 
     // API End Point /Account/login/google (We should remove Redundant Calls If Required)
     register(formData) {
-      const endPoint = environment.apiEndPoint + '/Account/Create';
+        const endPoint = environment.apiEndPoint + '/Account/Create';
 
-      return this.http.post(endPoint, formData).pipe(
-        map((res: TokenResponse) => {
-          return res;
-        }),
-        catchError((err) => {
-               return of(err);
-        })
-      );
+        return this.http.post(endPoint, formData).pipe(
+            map((res: TokenResponse) => {
+                return res;
+            }),
+            catchError((err) => {
+                return of(err);
+            })
+        );
     }
 
 
